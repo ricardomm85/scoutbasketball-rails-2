64329604382d3137492fd7250f044b810c4804e2
Ricardo MM <ricardomm85@users.noreply.github.com>
2024-01-06 18:38:31 +0000

    fix

--- test/controllers/players_controller_test.rb
+++ test/controllers/players_controller_test.rb
@@ -1,16 +1,18 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class PlayersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @player = players(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get players_url
     assert_response :success
   end
 
-  test "should show player" do
+  test 'should show player' do
     get player_url(@player)
     assert_response :success
   end
